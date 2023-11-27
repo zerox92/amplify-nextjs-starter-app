e1a2637088cfb83d7752a2ed3142351afc12fcc5
Zeeshan Anjum <zeerox92@gmail.com>
2023-11-27 16:47:39 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,11 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  console.log(context);
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Zeeshan Get started by editing&nbsp;
+          Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
       </div>
