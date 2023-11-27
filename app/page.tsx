1405405862dc017ea34cfd49ac222bb3bdcdcdaf
Zeeshan Anjum <zeerox92@gmail.com>
2023-11-27 16:37:45 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { runWithAmplifyServerContext } from '@/utils/amplifyServerUtils';
 
 export default function Home() {
   console.log(context);
